f0870257f2f62832c6aa9034f8e4756b83193dc6
angellofinetti <angellofinetti@gmail.com>
2026-09-20 23:25:29 -0500

    Fix typo in supabase url

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://ounvmeieamhnalpljgdf.supabase.co';
+const supabaseUrl = 'https://ounvmeieamhnaipljgdf.supabase.co';
 const supabaseAnonKey = 'sb_publishable_gp0vYhetAGgDkn1tnQ4eGA_fTUSv5Ct';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
